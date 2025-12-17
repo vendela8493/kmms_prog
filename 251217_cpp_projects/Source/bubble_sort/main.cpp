29aad42a30678491796216adbba27a9c85de9cf0
Виктория Разинова <razinova.vika00@mail.ru>
2025-12-17 13:55:50 +0300

    First commit

---
+++ 251217_cpp_projects/Source/bubble_sort/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "bubble_sort.hpp"
+
+using namespace std;
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+	const int m = sizeof(arr) / sizeof(arr[0]);
+
+    rva::bubble_sort(arr, m);
+
+    for (auto i : arr)
+        cout << i << " ";
+    return 0;
+}
