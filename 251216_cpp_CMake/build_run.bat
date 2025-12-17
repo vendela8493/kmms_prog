0ecd3d84f6e33fd01d5178426af4f9b0a52dd5e1
Виктория Разинова <razinova.vika00@mail.ru>
2025-12-17 03:58:36 +0300

    Remove unused files and update .gitignore

--- 251216_cpp_CMake/build_run.bat
+++ 251216_cpp_CMake/build_run.bat
@@ -6,7 +6,7 @@ set BUILD_SUFFIX=ninja
 chcp 65001
 
 set BUILD_FOLDER=build_%BUILD_SUFFIX%
-set SOURCE_FOLDER=Source
+set SOURCE_FOLDER=projects
 
 if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
 
@@ -15,16 +15,5 @@ cd %BUILD_FOLDER%
 cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
 cmake --build .
 
-set arr[0].file=run_bubble_sort.bat
-set arr[1].file=run_bubble_sort_mf.bat
-set arr[2].file=run_obuchaika.bat
-
-set arr[0].folder=bubble_sort
-set arr[1].folder=bubble_sort_mf
-set arr[2].folder=obuchaika
-
-for /L %%i in (0,1,2) do (
-	copy ..\%SOURCE_FOLDER%\!arr[%%i].folder!\!arr[%%i].file! .\!arr[%%i].folder!
-)
-
-copy ..\run_tests.bat .
+copy ..\%SOURCE_FOLDER%\bubble_sort\run_bubble_sort.bat .\bubble_sort
+copy ..\%SOURCE_FOLDER%\bubble_sort_mf\run_bubble_sort_mf.bat .\bubble_sort_mf
