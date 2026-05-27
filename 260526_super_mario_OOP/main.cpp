45064ee66558d31f63c78f82435900249adc71b8
Виктория Разинова <razinova.vika00@mail.ru>
2026-05-27 08:15:53 +0300

    Third Mario: just Tab

--- 260526_super_mario_OOP/main.cpp
+++ 260526_super_mario_OOP/main.cpp
@@ -4,96 +4,96 @@
 
 class GameObject
 {
-public:
-    float x, y;
-    float width, height;
-    float vertical_speed, horizontal_speed;
-    char kind;
-    bool is_flying;
+	public:
+		float x, y;
+		float width, height;
+		float vertical_speed, horizontal_speed;
+		char kind;
+		bool is_flying;
 
-    GameObject()
-    {
-        x = 0;
-        y = 0;
-        width = 0;
-        height = 0;
-        vertical_speed = 0;
-        horizontal_speed = 0.2;
-        kind = ' ';
-        is_flying = false;
-    }
+		GameObject()
+		{
+			x = 0;
+			y = 0;
+			width = 0;
+			height = 0;
+			vertical_speed = 0;
+			horizontal_speed = 0.2;
+			kind = ' ';
+			is_flying = false;
+		}
 
-    void set_object_pos(float obj_pos_x, float obj_pos_y)
-    {
-        x = obj_pos_x;
-        y = obj_pos_y;
-    }
+		void set_object_pos(float obj_pos_x, float obj_pos_y)
+		{
+			x = obj_pos_x;
+			y = obj_pos_y;
+		}
 
-    void init_object(float init_x, float init_y, float init_width, float init_height, char init_kind)
-    {
-        set_object_pos(init_x, init_y);
-        width = init_width;
-        height = init_height;
-        vertical_speed = 0;
-        horizontal_speed = 0.2;
-        kind = init_kind;
-        is_flying = false;
-    }
+		void init_object(float init_x, float init_y, float init_width, float init_height, char init_kind)
+		{
+			set_object_pos(init_x, init_y);
+			width = init_width;
+			height = init_height;
+			vertical_speed = 0;
+			horizontal_speed = 0.2;
+			kind = init_kind;
+			is_flying = false;
+		}
 };
 
 class GameMap
-{
-private:
-    static const int MAP_HEIGHT = 25;
-    static const int MAP_WIDTH = 80;
-    char map[MAP_HEIGHT][MAP_WIDTH + 1];
-	
-	bool object_within_map(int x, int y) const
-    {
-        return x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT;
-    }
-	
-public:
-    void clear_map()
-    {
-        for (int i = 0; i < MAP_WIDTH; i++)
-            map[0][i] = ' ';
-        map[0][MAP_WIDTH] = '\0';
-        
-        for (int j = 1; j < MAP_HEIGHT; j++)
-            for (int k = 0; k < MAP_WIDTH + 1; k++)
-                map[j][k] = map[0][k];
-    }
+	{
+	private:
+		static const int MAP_HEIGHT = 25;
+		static const int MAP_WIDTH = 80;
+		char map[MAP_HEIGHT][MAP_WIDTH + 1];
+		
+		bool object_within_map(int x, int y) const
+		{
+			return x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT;
+		}
+		
+	public:
+		void clear_map()
+		{
+			for (int i = 0; i < MAP_WIDTH; i++)
+				map[0][i] = ' ';
+			map[0][MAP_WIDTH] = '\0';
+			
+			for (int j = 1; j < MAP_HEIGHT; j++)
+				for (int k = 0; k < MAP_WIDTH + 1; k++)
+					map[j][k] = map[0][k];
+		}
 
-    void show_map()
-    {
-        COORD coord = { 0, 0 };
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-    
-        map[MAP_HEIGHT - 1][MAP_WIDTH - 1] = '\0';
-        
-        for (int j = 0; j < MAP_HEIGHT; j++)
-            std::cout << map[j] << '\n';
-    }
-	
-	void add_object_on_map(const GameObject& obj)
-    {
-        int int_x = (int)round(obj.x);
-        int int_y = (int)round(obj.y);
-        int int_width = (int)round(obj.width);
-        int int_height = (int)round(obj.height);
+		void show_map()
+		{
+			COORD coord = { 0, 0 };
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+		
+			map[MAP_HEIGHT - 1][MAP_WIDTH - 1] = '\0';
+			
+			for (int j = 0; j < MAP_HEIGHT; j++)
+				std::cout << map[j] << '\n';
+		}
+		
+		void add_object_on_map(const GameObject& obj)
+		{
+			int int_x = (int)round(obj.x);
+			int int_y = (int)round(obj.y);
+			int int_width = (int)round(obj.width);
+			int int_height = (int)round(obj.height);
 
-        for (int i = int_x; i < (int_x + int_width); i++)
-        {
-            for (int j = int_y; j < (int_y + int_height); j++)
-            {
-                if (object_within_map(i, j))
-                {
-                    map[j][i] = obj.kind;
-                }
-            }
-        }
-    }
+			for (int i = int_x; i < (int_x + int_width); i++)
+			{
+				for (int j = int_y; j < (int_y + int_height); j++)
+				{
+					if (object_within_map(i, j))
+					{
+						map[j][i] = obj.kind;
+					}
+				}
+			}
+		}
 };
 
 int main()
